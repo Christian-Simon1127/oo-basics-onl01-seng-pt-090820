100a4ab018387c136a1bf02c76ee61bf17ac15db
Christian Simon <csimon1127@gmail.com>
2020-09-23 20:21:15 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,10 @@
 #learn spec/01_book_spec.rb
 class Book 
+  attr_accessor :author_name, :page_count, :genre
   
-  def initialize)()
+  def initialize(title)
+    @title =  title
+  end 
   
 end 
 
